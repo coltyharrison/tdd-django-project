48a8f365348814a84605cea53698047c194b8c96
Colty Harrison <coltyharrison@gmail.com>
2018-07-12 22:52:36 -0700

    Integrate bootstrap

--- lists/tests.py
+++ lists/tests.py
@@ -65,7 +65,8 @@ class ListViewTest(TestCase):
     def test_passes_correct_list_to_template(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
-        response = self.client.get(f'lists/{correct_list.id}/add_item')
+        response = self.client.get(f'/lists/{correct_list.id}/')
+        self.assertEqual(response.context['list'], correct_list)
 
 
 class NewListTest(TestCase):
