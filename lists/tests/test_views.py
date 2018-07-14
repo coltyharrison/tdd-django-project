3a6d9e38257d13196fed5da70048609b89e36566
Colty Harrison <coltyharrison@gmail.com>
2018-07-14 15:29:23 -0700

    Enforce model validation in list_view

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -43,7 +43,7 @@ class ListViewTest(TestCase):
         correct_list = List.objects.create()
 
         self.client.post(
-            f'/lists/{correct_list.id}/add_item',
+            f'/lists/{correct_list.id}/',
             data={'item_text': 'A new item for an existing list'}
         )
 
@@ -57,11 +57,22 @@ class ListViewTest(TestCase):
         correct_list = List.objects.create()
 
         response = self.client.post(
-            f'/lists/{correct_list.id}/add_item',
+            f'/lists/{correct_list.id}/',
             data={'item_text': 'A new item for an existing list'}
         )
 
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
+    def test_validation_errors_end_up_on_lists_page(self):
+        list_ = List.objects.create()
+        response = self.client.post(
+            f'/lists/{list_.id}/',
+            data={'item_text': ''}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
 
 
 class NewListTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -11,7 +11,13 @@ def home_page(request):
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
     if request.method == "POST":
-        Item.objects.create(text=request.POST['item_text'], list=list_)
+        try:
+            item = Item.objects.create(text=request.POST['item_text'], list=list_)
+            item.full_clean()
+            item.save()
+        except ValidationError:
+            error = "You can't have an empty list item"
+            return render(request, 'list.html', {'error': error})
         return redirect(f'/lists/{list_.id}/')
     return render(request, 'list.html', {'list': list_})
 
